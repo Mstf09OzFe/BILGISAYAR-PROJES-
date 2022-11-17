e24a859813d14eaf4e55f6c3e9b49621afb87018
Mstf09OzFe <115787539+Mstf09OzFe@users.noreply.github.com>
2022-11-17 19:49:18 +0300

    Create fizik_kaldiraç.py

---
+++ fizik_kaldiraç.py
@@ -0,0 +1,6 @@
+x=int(input("lütfen cismin ağırlığını giriniz"))
+F=int(input("lütfen uygulayacağınız kuvveti giriniz"))
+if F>x:
+    print("cisim kaldırıldı")
+if x>F:
+    print("cisim kaldırılamadı")
