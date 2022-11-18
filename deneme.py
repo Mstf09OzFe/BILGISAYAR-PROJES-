58bb2d5b091eeac6d79c3db2eaa332b3d6fc787b
Mstf09OzFe <115787539+Mstf09OzFe@users.noreply.github.com>
2022-11-18 17:34:18 +0300

    Create deneme.py

---
+++ deneme.py
@@ -0,0 +1,8 @@
+m=int(input("lütfen cizmin ağırlığını kilogram cinsinden giriniz"))
+f=int(input("lütfenuygulanacak kuvveti newton cinsinden giriniz"))
+L=int(input("lütfen kadıracın uzunluğunu giriniz"))
+x=int(input("lütfen kaldıracın destek noktasının cisme uzaklığını giriniz"))
+if  f * (L-x) >= m * x * 10 :
+    print("cisim kaldırıldı ✓✓✓✓")
+if  f * (L-x) < m * x * 10 :
+    print("cisim kaldırılamadı  × × × × ")
