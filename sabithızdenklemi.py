758a8100dbd4210e7adbfd99b6720139c2f22a24
Mstf09OzFe <115787539+Mstf09OzFe@users.noreply.github.com>
2022-11-22 16:28:56 +0300

    Update sabithızdenklemi.py
    tamalar düzenlendi istenen veriler birimlendirildi

--- sabithızdenklemi.py
+++ sabithızdenklemi.py
@@ -1,17 +1,17 @@
 import math
 y = int ( input ( " cismin hızını bulmak için 1 , aldığı yolu bulmak için 2 , geçen zamanı bulmak için 3 yazınız"))
-if y < 2 :
-    x = int ( input ( " lütfen alınan yolu giriniz " ) ) 
-    t = int ( input ( " lütfen istediğiniz zamanı giriniz " ) )
+if  y == 1 :
+    x = int ( input ( " lütfen alınan yolu metre olarak giriniz  " ) ) 
+    t = int ( input ( " lütfen istediğiniz zamanı saniye olarak giriniz " ) )
     v =  x / t
     print ( v )
-if  1<y<3 :
-    t = int ( input ( " lütfen istediğiniz zamanı giriniz " ) )
-    v = int ( input ( " lütfen cismin hızını giriniz " ) ) 
+if  y == 2 :
+    t = int ( input ( " lütfen istediğiniz zamanı saniye olarak giriniz " ) )
+    v = int ( input ( " lütfen cismin hızını m/s olarak giriniz " ) ) 
     x = v * t
     print ( x )
-if y >= 3 :
-    v = int ( input ( " lütfen cismin hızını giriniz " ) ) 
-    x = int ( input ( " lütfen alınan yolu giriniz " ) ) 
+if  y == 3 :
+    v = int ( input ( " lütfen cismin hızını m/s olarak giriniz " ) ) 
+    x = int ( input ( " lütfen alınan yolu metre olarak giriniz " ) ) 
     t = x / v
-    print ( t )
+    print ( t )  
