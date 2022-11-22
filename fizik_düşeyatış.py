d18d71fc784db12bc3e9e0ff7e25f6726074a865
Mstf09OzFe <115787539+Mstf09OzFe@users.noreply.github.com>
2022-11-22 23:52:35 +0300

    Update fizik_düşeyatış.py
    düzenlemeler yapıldı hata giderildi

--- fizik_düşeyatış.py
+++ fizik_düşeyatış.py
@@ -1,8 +1,8 @@
-v0=int(input("ilk hızı giriniz"))
-t= int(input("girdiğiniz hızın 1/5'ini geçmeyecek bir saniye girirniz"))
-g=10
-if t>v0*1/5:
-    print("daha küçük bir saniyr değeri yazın")
-    t= int(input("girdiğiniz hızın 1/5'ini geçmeyecek bir saniye girirniz"))
-h= v0 * t-(1/2)*g*t**2
-print(h)
+v0 = int ( input ( " ilk hızı m/s olarak giriniz " ) )
+t = int ( input ( " girdiğiniz hızın 1/5'ini geçmeyecek bir saniye olarak girirniz " ) )
+g = 10
+if t > v0 * 1 / 5 :
+    print ( " daha küçük bir saniyr değeri yazın " )
+    t = int ( input ( " girdiğiniz hızın 1/5'ini geçmeyecek bir saniye girirniz " ) )
+h = v0 * t - ( 1 / 2 ) * g * t ** 2
+print ( h )
