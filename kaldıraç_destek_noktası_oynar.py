03056a70ff910c4d245ea399e934159390262bc4
Mstf09OzFe <115787539+Mstf09OzFe@users.noreply.github.com>
2022-12-30 18:31:52 +0300

    Update kaldıraç_destek_noktası_oynar.py
    while döngüsü katıldı

--- kaldıraç_destek_noktası_oynar.py
+++ kaldıraç_destek_noktası_oynar.py
@@ -1,8 +1,10 @@
-m=int(input("lütfen cizmin ağırlığını kilogram cinsinden giriniz"))
-f=int(input("lütfenuygulanacak kuvveti newton cinsinden giriniz"))
-L=int(input("lütfen kadıracın uzunluğunu giriniz"))
-x=int(input("lütfen kaldıracın destek noktasının cisme uzaklığını giriniz"))
-if  f * (L-x) >= m * x * 10 :
-    print("cisim kaldırıldı ✓✓✓✓")
-if  f * (L-x) < m * x * 10 :
-    print("cisim kaldırılamadı  × × × × ")
+run = True
+while run :
+   m=int(input("lütfen cizmin ağırlığını kilogram cinsinden giriniz"))
+   f=int(input("lütfenuygulanacak kuvveti newton cinsinden giriniz"))
+   L=int(input("lütfen kadıracın uzunluğunu giriniz"))
+   x=int(input("lütfen kaldıracın destek noktasının cisme uzaklığını giriniz"))
+   if  f * (L-x) >= m * x * 10 :
+        print("cisim kaldırıldı ✓✓✓✓")
+   if  f * (L-x) < m * x * 10 :
+        print("cisim kaldırılamadı  × × × × ")
