8ad718fd13313c2e4c619194c11766b0d79ea752
Mstf09OzFe <115787539+Mstf09OzFe@users.noreply.github.com>
2023-01-12 12:18:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,145 +1,103 @@
-# Gerekli kütüphaneler
-import button
-import pygame 
-import sys
-
-
-en= 940
-boy = 700
-kirmizi = (225,0,0)
-beyaz = (225,225,225)
-mavi = (0,0,225)
-siyah = (0,0,0)
-
+import pygame, sys
+from button import Button
 
 pygame.init()
 
-# ekran tanımlama
-screen = pygame.display.set_mode((en, boy))
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
 
-# Choose Font
-font = pygame.font.SysFont("arial", 50)
+BG = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\Dünya.jpg")
 
-# butonlar
-dünya_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\yuvarlakdünya.png")
-ay_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\yuvarlakay.png")
-cikis_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\button_quit.png")
-tofaş_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\tofaş.png")
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.SysFont("arial", 50)
 
-object1 = pygame.Rect(20, 50, 20, 50)
-object2 = pygame.Rect(20, 50, 20, 50)
-object3 = pygame.Rect(20, 50, 20, 50)
-object4 = pygame.Rect(20, 50, 20, 50)   
-obstacle = pygame.Rect(350, 500, 800, 50)
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-dünya_butonu = button.Button(90, 250, dünya_resmi, 1)
-ay_butonu = button.Button(700, 250, ay_resmi, 1)
-cikis_butonu = button.Button(400, 600, cikis_resmi, 1)
-tofaş_butonu = button.Button(735,0,tofaş_resmi,1)
-# States
-moving = False
-game_paused = True
-run = True
-menu_state = "main"
+        SCREEN.fill("black")
 
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
 
-# Event Loop
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONUP:
-            moving = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            object1.collidepoint(event.pos)
-            moving = True
-        elif event.type == pygame.MOUSEMOTION and moving:
-            object1.move_ip(event.rel)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                menu_state = "main"
-                screen.fill((255, 255, 255))
-                game_paused = True
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
 
-    if game_paused:
-        if menu_state == "main":
-            screen.fill((255,0,0))
-            background = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\uzaytrnindir.jpg") 
-            screen.blit(background, (0,0))
-            arkaplanterazi =pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\giriştahteravallisi.png")
-            screen.blit(arkaplanterazi,(500,550))
-            tofaş = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\tofaş.png")
-            screen.blit(tofaş,(735,0))
-            if dünya_butonu.draw(screen):
-                menu_state = "earth"
-                game_paused = False
-            if ay_butonu.draw(screen):
-                menu_state = "moon"
-                game_paused = False
-            if cikis_butonu.draw(screen):
-                run = False
-            if tofaş_butonu.draw(screen):
-                menu_state = "tofas"
-                game_paused = False
-    else:
-        if menu_state == "earth":
-            # background
-            background = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\Dünya.jpg") 
-            screen.blit(background, (0,0))
-            tahteravalli =pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\tahteravallirenli.png")
-            screen.blit(tahteravalli,(150,220))
-            küp_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\küp.png")
-            screen.blit(küp_resmi,(600,400))
-            ok_isareti =pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\ok1.png")
-            screen.blit(ok_isareti,(150,280))
-            geri_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\button_back.png")
-            geri_butonu = button.Button(0, 0, geri_resmi, 1)
-            if geri_butonu.draw(screen):
-                menu_state = "main" 
-                game_paused = True
-            # create planet text
-            
-            planet_text = font.render("50 N", True, (255, 0, 0))
-            screen.blit(planet_text, (200,230))
-            planet_text = font.render("4 kg", True, (255, 0, 0))
-            screen.blit(planet_text, (650,450))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
 
-        elif menu_state == "moon":
-            # background
-            background = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\aydunya.png")
-            screen.blit(background, (0, 0))
-            tahteravalli =pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\tahteravallirenli.png")
-            screen.blit(tahteravalli,(150,220))
-            küp_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\küp.png")
-            screen.blit(küp_resmi,(600,400))
-            ok_isareti =pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\ok1.png")
-            screen.blit(ok_isareti,(150,280))
-            geri_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\button_back.png")
-            geri_butonu = button.Button(0, 0, geri_resmi, 1)
-            if geri_butonu.draw(screen):
-                menu_state = "main" 
-                game_paused = True
+        pygame.display.update()
+    
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\yuvarlakdünya.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\button_options.png"), pos=(640, 400), 
+                            text_input=".", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\button_quit.png"), pos=(640, 550), 
+                            text_input=".", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
         
-            # create planet text
-            planet_text = font.render("50 N", True, (255, 0, 0))
-            screen.blit(planet_text, (200,230))
-            planet_text = font.render("4 kg", True, (255, 0, 0))
-            screen.blit(planet_text, (650,450))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
 
+        pygame.display.update()
 
-
-        elif menu_state == "tofas" :
-            # background
-            background = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\aydunya.png")
-            screen.blit(background, (0, 0))
-            geri_resmi = pygame.image.load("C:\\Users\\ozdem\\Desktop\\proje\\resimler\\button_back.png")
-            geri_butonu = button.Button(0, 0, geri_resmi, 1)
-            if geri_butonu.draw(screen):
-                menu_state = "main" 
-                game_paused = True
-
-
-    # update screen
-    pygame.display.update()
+main_menu()
